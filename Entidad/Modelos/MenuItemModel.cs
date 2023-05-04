e71064b0644bdec1deb2498b9cacce3861441bab
edg-laria <elaria@rlsis.com.ar>
2023-05-04 17:11:01 -0300

    add entidades

---
+++ Entidad/Modelos/MenuItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad.Modelos
+{
+  public class MenuItemModel
+    {
+        public string Icono;
+        public string Url;
+        public string Titulo;
+        public string Metodo;
+        public string Controller;
+    }
+}
---
+++ Entidad/Modelos/TreeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Entidad.Models
+{
+    public class TreeViewModel
+    {
+        public string text { get; set; }
+        public string href { get; set; }
+        public string tags { get; set; }
+        public ICollection<TreeViewModel> nodes { get; set; }
+       
+    }
+
+}
